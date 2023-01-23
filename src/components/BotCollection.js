a3ba0f69302843bd2be8852096520b45985d745b
TimothyKagondu <timothy.2k21@gmail.com>
2023-01-23 20:25:12 +0300

    added functionality to BotCollection.js

---
+++ src/components/BotCollection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import BotCard from "./BotCard"
+
+function BotCollection({ botCollection, action, removeCard }) {
+
+    const displayBotCards = botCollection.map(bot => {
+
+      return <BotCard bot={bot} action={action} removeCard={removeCard} />
+    })
+
+    return (
+
+        <div className="ui four column grid">
+          <div className="row">
+            You have completed your Bot Army. There are no more bots to collect.
+            {displayBotCards}
+          </div>
+        </div>
+      )
+    }
+
+    export default BotCollection;

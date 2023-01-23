be63eacd3e2f324089860766e2e3942b4b59740f
TimothyKagondu <timothy.2k21@gmail.com>
2023-01-23 20:09:06 +0300

    running json server

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -2,3 +2,14 @@ import React, { Component } from "react"
 import BotCollection from './BotCollection'
 import BotArmy from './YourBotArmy'
 import BotSpecs from '../components/BotSpecs'
+
+class BotsPage extends Component {
+    state = {
+      botCollection: [],
+      filteredCollection: [],
+      botArmy: [],
+      collectionVisible: true,
+      botSpecs: {},
+    }
+
+    
